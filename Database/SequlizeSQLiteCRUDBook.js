da286ab8608acf784a66d8eb351a835f7891ee9e
Pan153001 <s6506021621021@email.kmutnb.ac.th>
2024-02-07 11:02:10 +0700

    commit

--- Database/SequlizeSQLiteCRUDBook.js
+++ Database/SequlizeSQLiteCRUDBook.js
@@ -7,7 +7,7 @@ app.use(express.json());
 const sequelize = new Sequelize('database','username','password', {
     host: 'localhost',
     dialect: 'sqlite',
-    storage: './Database/SQBooks.sqlite'
+    storage: './Database/SQBook.sqlite'
 });
 
 const Book = sequelize.define('book',{
@@ -28,18 +28,10 @@ const Book = sequelize.define('book',{
 
 sequelize.sync();
 
-app.get('/books', (req, res) => {
-  Book.findAll().then(books => {
-      res.json(books);
-  }).catch(err => {
-    res.status(500).send(err);
-  });
-});
-
 app.get('/books/:id', (req, res) => {
     Book.findByPk(req.params.id).then(book => {
         if (!book){
-            res.status(404).send('Book not found');
+            res.status(404).send('book not found');
         } else {
             res.json(book);
         }
@@ -88,5 +80,5 @@ app.delete('/books/:id', (req, res) => {
     });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listen on port ${port}...`));
